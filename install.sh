f9945a87984aa1cde2b490db47c82afb507f7a01
Simon Sinding <thesinding@a.lan>
2016-11-15 22:50:38 +0100

    made the install.sh "automatic"

--- install.sh
+++ install.sh
@@ -3,6 +3,17 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+hash pastey 2>/dev/null || 
+{ 
+	dir=$(pwd)
+	echo "Pastey is not installed, but it's required"
+	echo "Cloning repo"
+	git clone https://github.com/TheSinding/pastey /tmp/pastey
+	cd /tmp/pastey
+	echo "Installing Pastey"
+	./install.sh
+	cd $dir
+}
 printf "Making klibbert executable"
 chmod +x klibbert
 printf "... ${BOLD}Done\n"
